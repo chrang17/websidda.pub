172d4a1fce24816dfd4d414bf4aaff46d7549e5d
chrang17 <102032853+chrang17@users.noreply.github.com>
2022-05-16 13:56:41 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+function myFunction(){
+    document.getElementById("bttn1").style.color = "red";
+    
+}
+
+function MyFunction(){
+    document.getElementById("btnn2").style.color = "red";
+}
+
+function myfunction(){
+    document.getElementById("bttn3").style.color = "red";
+        
+}
+
+function Myfunction(){
+    document.getElementById("bttn4").style.color = "red";
+    
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
